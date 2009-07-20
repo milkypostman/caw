884beb0532a50bf3ee62079781532dba8955d1a0
dcurtis <dcurtis@cs.uiowa.edu>
2009-07-19 19:03:49 -0500

    initial commit

---
+++ pynel.py
@@ -0,0 +1,361 @@
+#!/usr/bin/python
+
+from ppmodule import ppinit, ppshade, ppicon, ppfont, ppfontsize, ppclear
+import Xlib
+import Xlib.display
+import Xlib.protocol.event
+import collections
+import heapq
+import select
+import time
+import re
+import operator
+import itertools
+import math
+from Xlib import Xatom, Xutil, X
+
+FONT = "Terminus-8"
+
+class Widget(object):
+    def __init__(self):
+        super(Widget, self).__init__()
+        self.width = 0
+        self.x = 0
+        self.parent = None
+
+    def setup(self):
+        """ reimplement in subclass """
+
+    def draw(self):
+        """ reimplement in subclass """
+        pass
+
+class Text(Widget):
+    def __init__(self, text="undefined", color=None):
+        super(Text, self).__init__()
+        self.text = text
+        self.color = color
+
+    def setup(self):
+        self.width = self.parent.text_width(self.text)
+
+    def draw(self):
+        self.parent.draw_text(self.x, self.text, self.color)
+
+
+class CPU(Widget):
+    _initialized = False
+    _widgets = collections.defaultdict(list)
+
+    def __init__(self, cpu=0):
+        super(CPU, self).__init__()
+        self.cpu = cpu
+
+    def setup(self):
+        self.data = collections.defaultdict(int)
+
+        if not CPU._initialized:
+            CPU._clsinit(self.parent)
+
+        CPU._widgets[self.cpu].append(self)
+
+    @classmethod
+    def _clsinit(cls, parent):
+        cls._re = re.compile('^cpu')
+        cls._sep = re.compile('[ *]*')
+        cls._file = open('/proc/stat', 'r')
+        cls._cache = {}
+        cls._parent = parent
+
+        cls._update(0)
+
+        cls._initialized = True
+
+    @classmethod
+    def _update(cls, timeout=3):
+        print "CPU:_update"
+        cls._file.seek(0)
+        i = 0
+        for line in cls._file:
+            if cls._re.match(line):
+                info = cls._sep.split(line)
+                active = reduce(operator.add, itertools.imap(int, info[1:4]))
+                total = active + int(info[4])
+
+                try:
+                    difftotal = total - cls._cache[i]['total']
+                    diffactive = active - cls._cache[i]['active']
+                    cls._cache[i]['usage'] = math.floor((float(diffactive) / difftotal) * 100)
+                except KeyError:
+                    cls._cache[i] = {}
+                except ZeroDivisionError:
+                    cls._cache[i]['usage'] = 0
+
+                cls._cache[i]['total'] = total
+                cls._cache[i]['active'] = active
+
+                for w in cls._widgets.get(i, []):
+                    w.data = cls._cache[i]
+                    w.parent.update()
+
+                i += 1
+
+        cls._parent.schedule(timeout, cls._update)
+
+    def _get_data(self):
+        return self._data
+
+    def _set_data(self, data):
+        self._data = data
+        self.width = self.parent.text_width("%d%%" % self._data['usage'])
+
+    data = property(_get_data, _set_data)
+
+    def draw(self):
+        print "DRAWING:", self.x
+        print self._data
+        self.parent.draw_text(self.x, "%d%%" % self._data['usage'])
+
+class Clock(Widget):
+    def __init__(self, format="%Y.%m.%d %H:%M:%S"):
+        super(Clock, self).__init__()
+        self.format = format
+
+    def setup(self):
+        self.update()
+
+    def update(self):
+        print "Clock: update"
+        self.text = time.strftime(self.format)
+        self.width = self.parent.text_width(self.text)
+        self.parent.update();
+        self.parent.schedule(1, self.update)
+
+    def draw(self):
+        self.parent.draw_text(self.x, self.text, 0xffffff)
+
+
+class Pynel(object):
+    def __init__(self):
+        super(Pynel, self).__init__()
+        self.display = Xlib.display.Display()
+        self.screen = self.display.screen()
+        self.root = self.screen.root
+
+        self.events = collections.defaultdict(list)
+        self.atoms = collections.defaultdict(list)
+        self._timers = []
+
+        self.width = self.screen.width_in_pixels
+        self.height = 10
+        self.x = 0
+        self.y = self.screen.height_in_pixels - self.height
+
+        self.bg = 0xffffff
+        self.fg = 0x000000
+        self.shade = 100
+
+        self.left = []
+        self.right = []
+
+        self.root_map = None
+
+        self.window = self.screen.root.create_window(self.x, self.y,
+            self.width, self.height, 
+            0, self.screen.root_depth, window_class=X.InputOutput,
+            visual=X.CopyFromParent, colormap=X.CopyFromParent, 
+            event_mask=(X.ExposureMask|X.ButtonPressMask|X.ButtonReleaseMask|X.EnterWindowMask))
+
+        ppinit(self.window.id, FONT)
+
+        self._set_props()
+        self._update_struts()
+        self.root.change_attributes(event_mask=(X.PropertyChangeMask))
+        self.window.map()
+        self.display.flush()
+
+        self.events[X.Expose].append(self._redraw)
+        self.events[X.PropertyNotify].append(self._property_notify)
+        self.atoms[self._XROOTPMAP_ID].append(self._update_background)
+
+    def _property_notify(self, e):
+        for func in self.atoms[e.atom]:
+            func(e)
+
+    def _set_props(self):
+        """ Set necessary X atoms and panel window properties """   
+        self._WIN_STATE = self.display.intern_atom("_WIN_STATE")
+        self._XROOTPMAP_ID = self.display.intern_atom("_XROOTPMAP_ID")
+        self._NET_WM_NAME = self.display.intern_atom("_NET_WM_NAME")
+        self._NET_WM_DESKTOP = self.display.intern_atom("_NET_WM_DESKTOP")
+        self._NET_WM_WINDOW_TYPE = self.display.intern_atom("_NET_WM_WINDOW_TYPE")
+        self._NET_WM_WINDOW_TYPE_DOCK = self.display.intern_atom("_NET_WM_WINDOW_TYPE_DOCK")
+        self._NET_WM_STATE = self.display.intern_atom("_NET_WM_STATE")
+        self._NET_WM_STATE_STICKY = self.display.intern_atom("_NET_WM_STATE_STICKY")
+        self._NET_WM_STATE_SKIP_PAGER = self.display.intern_atom("_NET_WM_STATE_SKIP_PAGER")
+        self._NET_WM_STATE_SKIP_TASKBAR = self.display.intern_atom("_NET_WM_STATE_SKIP_TASKBAR")
+        self._NET_WM_STATE_ABOVE = self.display.intern_atom("_NET_WM_STATE_ABOVE")
+        self._NET_WM_STRUT = self.display.intern_atom("_NET_WM_STRUT")
+        self._NET_WM_STRUT_PARTIAL = self.display.intern_atom("_NET_WM_STRUT_PARTIAL")
+
+        self._MOTIF_WM_HINTS = self.display.intern_atom("_MOTIF_WM_HINTS")
+
+        self.window.set_wm_name("Pynel")
+        self.window.set_wm_class("pynel","Pynel") 
+
+        self.window.set_wm_hints(flags=(Xutil.InputHint|Xutil.StateHint),
+            input=0, initial_state=1)
+
+        self.window.set_wm_normal_hints(flags=(
+            Xutil.PPosition|Xutil.PMaxSize|Xutil.PMinSize),
+            min_width=10, min_height=10,
+            max_width=self.width, max_height=10)
+
+        self.window.change_property(self._NET_WM_DESKTOP, Xatom.CARDINAL, 32,
+                [0xffffffffL])
+        self.window.change_property(self._NET_WM_NAME, Xatom.STRING, 8, 
+                "Pynel\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
+
+        self.window.change_property(self._WIN_STATE, Xatom.CARDINAL, 32, [1])
+
+        self.window.change_property(self._NET_WM_WINDOW_TYPE,
+            Xatom.ATOM, 32, [self._NET_WM_WINDOW_TYPE_DOCK])
+
+        self.window.change_property(self._NET_WM_STATE, Xatom.ATOM, 32,
+                [1, self._NET_WM_STATE_STICKY])
+
+        #self.window.change_property(self._MOTIF_WM_HINTS,
+        #        self._MOTIF_WM_HINTS, 32, [0x2, 0x0, 0x0, 0x0, 0x0])
+
+        self.window.change_property(self._NET_WM_STATE, Xatom.ATOM, 32,
+                [1, self._NET_WM_STATE_SKIP_PAGER])
+        self.window.change_property(self._NET_WM_STATE, Xatom.ATOM, 32,
+                [1, self._NET_WM_STATE_SKIP_TASKBAR])
+        self.window.change_property(self._NET_WM_STATE, Xatom.ATOM, 32,
+                [1, self._NET_WM_STATE_ABOVE])
+
+    def _update_struts(self):
+        self.window.change_property(self._NET_WM_STRUT, Xatom.CARDINAL, 32,
+                [0, 0, self.height, 0])
+        self.window.change_property(self._NET_WM_STRUT_PARTIAL, Xatom.CARDINAL, 32,
+                [0, 0, 0, self.height, 0, 0, 0, 0, 0, 0, 0, self.width])
+
+    def send_event(self, win, ctype, data, mask=None):
+        data = (data+[0]*(5-len(data)))[:5]
+        ev = Xlib.protocol.event.ClientMessage(window=win, client_type=ctype, data=(32,(data)))
+
+        if not mask:
+            mask = (X.SubstructureRedirectMask|X.SubstructureNotifyMask)
+        self.root.send_event(ev, event_mask=mask)
+
+
+    def schedule(self, timeout, func):
+        heapq.heappush(self._timers, (timeout + int(time.time()), func))
+
+    def _init_widgets(self):
+        for w in self.left:
+            w.parent = self
+            w.setup()
+
+        for w in self.right:
+            w.parent = self
+            w.setup()
+
+    def mainloop(self):
+        self._init_widgets()
+        self._update_background()
+
+        poll = select.poll()
+        poll.register(self.display.display.socket, select.POLLIN)
+
+        timeout = 0
+        while True:
+            if self._update:
+                self._redraw()
+                self._update = False
+
+            p = poll.poll(timeout*1000)
+            if p:
+                while self.display.pending_events():
+                    e = self.display.next_event()
+                    print e
+                    for func in self.events[e.type]:
+                        func(e)
+
+            if len(self._timers) > 0:
+                now = time.time()
+                while self._timers[0][0] <= now:
+                    timeout, func = heapq.heappop(self._timers)
+                    func()
+
+                timeout = max(self._timers[0][0] - time.time(), 1)
+            else:
+                timeout = -1
+
+
+    def _update_background(self, *_):
+        """ Check and update the panel background if necessary """
+        root_map = self.root.get_full_property(self._XROOTPMAP_ID, Xatom.PIXMAP)
+
+        if hasattr(root_map, "value"):
+            root_map = root_map.value[0]
+        else:
+            root_map = self.root.id
+
+        if self.root_map != root_map:
+            self.root_map = root_map
+            r = self.bg >> 16
+            g = (self.bg >> 8) & 0xff
+            b = self.bg & 0xff
+            ppshade(self.window.id, root_map, 
+                    self.x, self.y, self.width, self.height,
+                    r, g, b, self.shade)
+        self.update()
+
+    def _redraw(self, *_):
+        print "drawing"
+        self._clear_panel(0, 0, self.width, self.height)
+        space = self.width
+        leftx = 0
+        first = True
+        for w in self.left:
+            w.x = leftx
+            w.draw()
+            leftx += w.width
+            space -= w.width
+
+        rightx = self.width
+        for w in self.right:
+            w.x = rightx - w.width
+            w.draw()
+            rightx -= w.width
+            space -= w.width
+
+    def _clear_panel(self, x1, y1, x2, y2):
+        ppclear(self.window.id, x1, y1, x2, y2)
+
+    def draw_text(self, x, text, color=None):
+        if color is None:
+            color = self.fg
+        ppfont(self.window.id, color, x, self.height, 0, text)
+
+    def text_width(self, text):
+        return ppfontsize(text)
+
+    def update(self):
+        """ set the flag so that we redraw """
+        self._update = True
+
+
+if __name__ == "__main__":
+    pynel = Pynel()
+
+    #pynel.left.append(Desktop())
+    pynel.right.append(Clock())
+    pynel.right.append(Text(" | ", 0x00dd00))
+    pynel.right.append(CPU(2))
+    pynel.right.append(Text(" / ", 0x000000))
+    pynel.right.append(CPU(1))
+
+    pynel.mainloop()
+
