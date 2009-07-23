7fc2a778dd818d191e2edce221cf4fc00a03140b
dcurtis <dcurtis@cs.uiowa.edu>
2009-07-22 20:13:07 -0500

    looking at the old branch reformed!

--- pynel.py
+++ pynel.py
@@ -15,6 +15,12 @@ import itertools
 import math
 from Xlib import Xatom, Xutil, X
 
+try:
+    import alsaaudio
+except ImportError:
+    alsaaudio = None
+import ossaudiodev
+
 FONT = "Terminus-8"
 
 class Widget(object):
@@ -32,6 +38,83 @@ class Widget(object):
         """ reimplement in subclass """
         pass
 
+class Volume:
+    def __init__(self, device='Master', med=30, high=70, step=1, driver='alsa'):
+        self.device = device
+        self.med = med
+        self.high = high
+        self.step = step
+        if driver == 'alsa' and alsaaudio is None :
+            driver = 'oss'
+
+        self.driver = driver
+
+    def init(self):
+        getattr(self, '_init_' + self.driver)()
+        self.min = 0
+        self.max = 100
+
+        self.widget = wmii.Widget(self.name, self.bar)
+        wmii.register_widget(self.widget)
+        self.widget.clicked = self.widget_clicked
+
+        self.fglow = wmii.colors.get('volume_fglow', wmii.colors['normfg'])
+        self.fgmed = wmii.colors.get('volume_fgmed', wmii.colors['normfg'])
+        self.fghigh = wmii.colors.get('volume_fghigh', wmii.colors['normfg'])
+
+        self.update()
+
+    def _init_oss(self):
+        self.device_mask = getattr(ossaudiodev, "SOUND_MIXER_%s" % self.device.upper(), None)
+        if self.device_mask is None:
+            self.device_mask = getattr(ossaudiodev, "SOUND_MIXER_VOLUME")
+
+        self.mixer = ossaudiodev.openmixer()
+
+    def _init_alsa(self):
+        self.mixer = alsaaudio.Mixer(self.device)
+        self.min, self.max = self.mixer.getrange()
+
+    def _update_alsa(self):
+        vol = self.mixer.getvolume()
+        self.current = reduce(operator.add, vol) / len(vol)
+        return round((float(self.current) / self.max) * 100)
+
+    def _update_oss(self):
+        vol = self.mixer.get(self.device_mask)
+        self.current = reduce(operator.add, vol) / len(vol)
+        return round((float(self.current) / self.max) * 100)
+
+    def update(self):
+        self._update()
+        wmii.schedule(5, self.update)
+
+    def _update(self):
+        percent = getattr(self, '_update_' + self.driver)()
+        fg = self.fglow
+        if percent > self.high:
+            fg = self.fghigh
+        elif percent > self.med:
+            fg = self.fgmed
+
+        self.widget.fg = fg
+        self.widget.show("%d%%" % percent)
+
+    def _set_alsa(self, value):
+        self.mixer.setvolume(value)
+
+    def _set_oss(self, value):
+        self.mixer.set(self.device_mask, (value,value))
+
+    def widget_clicked(self, button):
+        if button == 5:
+            newval =  max(self.current-self.step, self.min)
+            getattr(self, "_set_" + self.driver)(newval)
+            self._update()
+        elif button == 4:
+            newval =  min(self.current+self.step, self.max)
+            getattr(self, "_set_" + self.driver)(newval)
+            self._update()
 class Desktop(Widget):
     def __init__(self, current_fg=None, fg=None, current_bg=None, bg=None):
         self.desktops = []
@@ -390,8 +473,8 @@ class Pynel(object):
 
         self.window.change_property(self._WIN_STATE, Xatom.CARDINAL, 32, [1])
 
-        self.window.change_property(self._NET_WM_WINDOW_TYPE,
-            Xatom.ATOM, 32, [self._NET_WM_WINDOW_TYPE_DOCK])
+        #self.window.change_property(self._NET_WM_WINDOW_TYPE,
+        #    Xatom.ATOM, 32, [self._NET_WM_WINDOW_TYPE_DOCK])
 
         self.window.change_property(self._NET_WM_STATE, Xatom.ATOM, 32,
                 [1, self._NET_WM_STATE_STICKY])
