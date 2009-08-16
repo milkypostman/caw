37edb334b2cad37903fd622cd6ee57ad821d0c6c
dcurtis <dcurtis@cs.uiowa.edu>
2009-08-15 21:25:26 -0500

    Fix to Wifi widget and missing path.

--- caw/widgets/wifi.py
+++ caw/widgets/wifi.py
@@ -19,25 +19,33 @@ class Wifi(caw.widget.Widget):
 
         self.normal_fg = kwargs.get('normal_fg', fg)
         self.width_hint = 0
+        self.path = '/sys/class/net/%s/wireless/link' %self.adapter
+        self.file = None
+        self.data = 0
 
     def init(self, parent):
         super(Wifi, self).init(parent)
-        path = '/sys/class/net/%s/wireless/link' %self.adapter
-        if os.path.isfile(path):
-            self.file = open(path, 'r')
-            self.update()
-        else:
-            print "Error: path does not exist.", path
+        self.update()
 
     def update(self):
-        self.file.seek(0)
-        self.data = int(self.file.read())
+        if self.file is None:
+            if os.path.isfile(self.path):
+                self.file = open(self.path, 'r')
+            else:
+                print "Wifi Error: path does not exist.", self.path
+        else:
+            try:
+                self.file.seek(0)
+                self.data = int(self.file.read())
+            except IOError:
+                self.file = None
+                self.data = -1
+
         self.width_hint = self.parent.text_width(str(self.data))
         self.parent.schedule(5, self.update)
 
     def draw(self):
         data = self.data
         self.parent.draw_text(str(data) , self.normal_fg)
-
 
 
